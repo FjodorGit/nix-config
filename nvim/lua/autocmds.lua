76d00716b63cb8888f40cdbe0ef33192350de8a0
FjodorGit <f.kholodkov@gmail.com>
2026-03-29 12:36:02 +0000

    remove double formatting and lazy lock for better transferability

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,11 +1,3 @@
--- Format on save/buffer write
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*',
-  callback = function(args)
-    require('conform').format { bufnr = args.buf }
-  end,
-})
-
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
