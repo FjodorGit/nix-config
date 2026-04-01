de001bec31d3e8df532a535fda2a6564d8d36452
FjodorGit <f.kholodkov@gmail.com>
2026-04-01 18:44:59 +0000

    ipv6 warp setup

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -38,6 +38,41 @@
       ];
       "2606:50c0:8000::154" = [ "github.githubassets.com" ];
     };
+
+    firewall.checkReversePath = "loose";
+    wg-quick.interfaces.wgcf = {
+      privateKeyFile = "/home/fjk/wgcf-private-key";
+
+      address = [ "172.16.0.2/32" ];
+
+      dns = [
+        "1.1.1.1"
+        "1.0.0.1"
+        "2606:4700:4700::1001"
+        "2606:4700:4700::1111"
+      ];
+      mtu = 1420;
+
+      table = "off";
+      postUp = ''
+        ip route add default dev wgcf table 51820
+        ip rule add not fwmark 51820 table 51820
+        ip rule add table main suppress_prefixlength 0
+      '';
+      postDown = ''
+        ip rule del not fwmark 51820 table 51820
+        ip rule del table main suppress_prefixlength 0
+      '';
+
+      peers = [
+        {
+          publicKey = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
+          endpoint = "[2606:4700:d0::a29f:c001]:2408";
+          allowedIPs = [ "0.0.0.0/0" ];
+          persistentKeepalive = 25;
+        }
+      ];
+    };
   };
 
   systemd.network = {
